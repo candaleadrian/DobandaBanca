843d413ac93f6c07ffe8a8ffa10e8398e5cafef3
candaleadrian <candale.adrian@gmail.com>
2015-11-17 22:11:57 +0200

    CalculFunctie

---
+++ DobandaBancaDescendenta/DobandaBancaDescendenta/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DobandaBancaDescendenta
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
+class Clasa
+{
+    int calculatorRata(int sumaImprumutata, Decimal dobanda, int nrLuni)
+    {
+        Decimal sumaDatorataPeLunaFaraDob = sumaImprumutata / nrLuni;
+
+        Decimal totalDobanda = sumaImprumutata * dobanda / 100; 
+
+        return 4;
+    }
+}
